eafcfe59194bbef7a88db86608a332f1be620234
AILalev22 <ailalev22@codingburgas.bg>
2024-02-13 21:29:47 +0200

    Add start menu

---
+++ UniBox/Includes/Unibox.h
@@ -0,0 +1,228 @@
+#pragma once
+#include "raylib.h"
+#include <math.h>
+#include <map>
+#include <string>
+#include <sstream>
+#include <cstdlib>
+#include <vector>
+#include <algorithm>
+#pragma warning(disable: 6387) // remove a C6387 warning which is unnecessary, const char* '0' instead of "0000", we are using only strings not single chars
+#pragma warning(disable: 4305) // const double to float data loss warning (unnecessary)
+#pragma warning(disable: 4244) // float to int data loss conversion warning (unnecessary)
+using namespace std;
+const int screenWidth = 1745; //800, 1745
+const int screenHeight = 895; //450
+/*
+ à = avarage distance to sun
+ e = warping a planet
+ perihelionAngle = closest point of each planet to the sun
+*/
+namespace sun
+{
+	const double a = 0.0;
+	const double e = 0.0;
+	const double perihelionAngle = 0.0;
+}
+namespace mercury
+{
+	const double a = 63.9; // 57.9, 50.9
+	const double e = 0.2056;
+	const double perihelionAngle = 77.46;
+}
+namespace venus
+{
+	const double a = 94.2; //108.2,90.2
+	const double e = 0.0067;
+	const double perihelionAngle = 131.77;
+}
+namespace earth
+{
+	const double a = 128.6;  //149.6
+	const double e = 0.0167;
+	const double perihelionAngle = 102.94;
+}
+namespace mars
+{
+	const double a = 178.9;  //227.9,166.9
+	const double e = 0.0334;  //0,0934
+	const double perihelionAngle = 336.04;
+}
+namespace jupiter
+{
+	const double a = 229.6; // 778.6
+	const double e = 0.019; //0.049
+	const double perihelionAngle = 14.75; //14.75
+}
+namespace saturn
+{
+	const double a = 287.5; // 1433.5, 272.5
+	const double e = 0.0065; //0.0565, 0.0365
+	const double perihelionAngle = 92.43; //92.43
+}
+namespace uranus
+{
+	const double a = 370.5; //2872.5, 335.5, 350.5
+	const double e = 0.0164; // 0.464, 0.0164, 
+	const double perihelionAngle = 170.96; // 170.96
+}
+namespace neptune
+{
+	const double a = 435.1; //4495.1, 400.1
+	const double e = 0.0106; //0.0106, 0.0106
+	const double perihelionAngle = 44.97; //44.97
+}
+
+// draw orbit
+void draw_orbit(const double, const double, const double);
+
+// alignments of different planets and the sun
+
+namespace align_sun
+{
+	int down = 385;
+	int left = 830; // 838
+	float angle = 0.f;
+}
+namespace align_mercury
+{
+	int down = 390;
+	int left = 800;
+	float angle = 77.46f;
+}
+namespace align_venus
+{
+	int down = 400;
+	int left = 759;
+	float angle = 0.f;
+}
+namespace align_earth
+{
+	int down = 400;
+	int left = 718;
+	float angle = 0.f;
+}
+namespace align_mars
+{
+	int down = 405;
+	int left = 659;
+	float angle = 0.f;
+}
+namespace align_jupiter
+{
+	int down = 405;
+	int left = 603;
+	float angle = 0.f;
+}
+namespace align_saturn
+{
+	int down = 375;
+	int left = 494;
+	float angle = 0.f;
+}
+namespace align_uranus
+{
+	int down = 410;
+	int left = 510;
+	float angle = 0.f;
+}
+namespace align_neptune
+{
+	int down = 410;
+	int left = 390;
+	float angle = 0.f;
+}
+
+// alignments of stats menu and its elements: left is x ,down is y
+// average k is 60 ( average difference between all downs), most of them are decremented by 2 to make the text look better
+// stats
+const int left_stats = 1395;
+const int align_stats_down[9] = { 95,153,213,273,333,393,453,513,573 };
+// length
+const int left_length[9] = { 1458,1450,1450,1446,1450,1443,1442,1443,1442 };
+const int align_length_down[9] = { 171,229,290,349,408,469,529,590,650 };
+// temperature
+const int left_temperature[9] = { 1467,1472,1470,1484,1480,1467,1465,1465,1464 };
+const int align_temperature_down[9] = { 108,166,226,286,346,406,465,526,585 };
+// speed
+const int left_speed[9] = { 1460,1454,1450,1456,1455,1455,1460,1456,1460 };
+const int align_speed_down[9] = { 244,302,362,422,482,542,602,662,722 };
+
+// text size in stats
+#define text_size 28
+
+// we are using a key value, in this case a string to find each temperature and speed (float value)
+// lengths is just const char* to const char*
+map<string, float> temperatures =
+{
+	{"Sun",5500.0f},
+	{"Mercury",430.0f},
+	{"Venus",470.0f},
+	{"Earth",15.0f},
+	{"Mars",20.0f},
+	{"Jupiter",-145.0f},
+	{"Saturn",-178.0f},
+	{"Uranus",-224.0f},
+	{"Neptune",-220.0f}
+};
+map <const char*, const char*> lengths =
+{
+	{"Sun","1.4Mkm"},
+	{"Mercury","4880km"},
+	{"Venus","12104km"},
+	{"Earth","12742km"},
+	{"Mars","6779km"},
+	{"Jupiter","98982km"},
+	{"Saturn","116460km"},
+	{"Uranus","50724km"},
+	{"Neptune","49244km"}
+};
+map <const char*, float> speeds =        // theoretical speeds in real life
+{
+	{"Sun",10000.f},
+	{"Mercury", 170503.f},
+	{"Venus", 126074.f},
+	{"Earth", 107218.f},
+	{"Mars", 86667.f},
+	{"Jupiter", 47002.f},
+	{"Saturn", 34701.f},
+	{"Uranus", 24477.f},
+	{"Neptune", 19556.f}
+};
+namespace actual_speed // float value speed
+{
+	static float mercury_speed = 3.f; // 1
+	static float venus_speed = 2.f; //2
+	static float earth_speed = 1.f; //3
+	static float mars_speed = 0.5f; //4
+	static float jupiter_speed = 0.3f; //5
+	static float saturn_speed = 0.08f; //6
+	static float uranus_speed = 0.05f; //7
+	static float neptune_speed = 0.02f; //8
+
+}
+vector <bool> draw_on_orbit(9); // checkers if pluton needs to be in the program
+static bool black_hole_disapppear[9];
+//static float speed_pluton[9]
+void draw_planet(Texture2D, int&, int&, float&); // draw a texture using DrawTexturePro
+
+Vector2 rotate_point(float&, float, float, float, float);
+
+Vector2 rotate_point_back(float& angle, float a, float e, float perihelionAngle, float speed);
+
+void rotate_planet(float&, float, float, float, float, int&, int&);
+
+void rotate_planet_back(float& angle, float a, float e, float perihelionAngle, float speed, int& left, int& down);
+
+string convert_float_to_str(float); // convert float to string
+
+// void stats_show(Texture2D planet, const char* name,Texture2D Close,const int align_left, const int align_down);
+
+void menu_show(Texture2D, Texture2D, Texture2D, Texture2D, Texture2D, Texture2D); // show menu if clicked on info button and hover on the planets to see their stats
+void functions_menu(Texture2D f_menu, Texture2D Checker, Texture2D exit_button, Texture2D checker_exit_button, Texture2D exit_menu, Texture2D options_checker, Texture2D Mercury, Texture2D Venus, Texture2D Earth, Texture2D Mars, Texture2D Jupiter, Texture2D Saturn, Texture2D Uranus, Texture2D Neptune, Texture2D Sun, Texture2D pluton, Texture2D add_planet_menu, Texture2D cancel_add_planet, Texture2D cancel_add_planet_blank);
+static int info_check; // check either to show or close the stats menu, by making chnages with its value under certain conditions
+static int check1, check2, check3, check4;
+
+
+
+	
---
+++ UniBox/UniBox.cpp
@@ -0,0 +1,131 @@
+#include "Includes/Unibox.h"
+
+int main()
+{
+    SetTargetFPS(60);
+
+    InitWindow(screenWidth, screenHeight, "Unibox"); // set screen size and name of .exe file
+    Texture2D background = LoadTexture("Images/galaxy.png"); // galaxy background
+    Texture2D stats = LoadTexture("Images/stats.png"); // show stats of pressed planet
+    Texture2D Sun = LoadTexture("Images/sun100x100.png"); // sun picture
+    Texture2D Mercury = LoadTexture("Images/mercury45x45.png"); // mercury picture
+    Texture2D Venus = LoadTexture("Images/venus50x50.png"); // venus picture
+    Texture2D Earth = LoadTexture("Images/earth47x47.png"); // earth picture
+    Texture2D Mars = LoadTexture("Images/mars48x48.png"); // mars picture
+    Texture2D Jupiter = LoadTexture("Images/jupiter49x49.png"); // jupiter picture
+    Texture2D Saturn = LoadTexture("Images/saturn121x138.png"); // saturn image
+    Texture2D Uranus = LoadTexture("Images/uranus53x52.png"); // uranus image
+    Texture2D Neptune = LoadTexture("Images/neptune70x128.png"); // neptune image
+    Texture2D blank_info = LoadTexture("Images/blank_info.png"); // cancel stats button blank
+    Texture2D start = LoadTexture("images/start_screen.png"); // start menu screen
+    Texture2D new_game = LoadTexture("Images/new_game_button.png"); // new game button
+    Texture2D quit = LoadTexture("Images/quit_button.png"); // quit button
+    Texture2D info = LoadTexture("Images/stats_icon.png"); // stats info button
+    Texture2D menu = LoadTexture("Images/stats_menu.png"); // stats menu picture
+    Texture2D functions = LoadTexture("Images/functions_menu.png"); // function options on the left
+    Texture2D checker = LoadTexture("Images/rectangle46.png");
+    Texture2D Checker = LoadTexture("Images/rectangle49.png"); // functions buttons checker
+    Texture2D exit_button = LoadTexture("Images/exit_options.png"); // exit button texture
+    Texture2D checker_exit_button = LoadTexture("Images/checker_exit.png"); // checker to see if exit options are pressed   
+    Texture2D exit_menu = LoadTexture("Images/exit_menu.png"); // exit menu options
+    Texture2D options_checker = LoadTexture("Images/exit_options_checker.png");// checker if any of the exit options are checked
+
+    Texture2D pluton = LoadTexture("Images/pluton.png"); // pluton picture    
+    Texture2D add_planet_menu = LoadTexture("Images/add_planet_menu2.png"); // add planet menu
+    Texture2D cancel_add_planet = LoadTexture("Images/cancel_button.png");// cancel button
+    Texture2D cancel_add_planet_blank = LoadTexture("Images/add_planet_cancel_blank_button.png");
+    Texture2D pluton_in_stats = LoadTexture("Images/mercury_in_stats_menu.png");
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        DrawTexture(start, 0, 0, WHITE); // background screen 
+        DrawTexture(new_game, 710, 500, WHITE); // new game button
+        DrawTexture(quit, 814, 620, WHITE); // quit button
+
+
+        bool hover_new_game = CheckCollisionPointRec(GetMousePosition(), // checker if new game button is hovered
+            { 710, 500, (float)new_game.width, (float)new_game.height }); // 710 is left align, 500 is down align
+
+        bool hover_quit = CheckCollisionPointRec(GetMousePosition(), // checker if quit button is hovered
+            { 814, 620, (float)quit.width, (float)quit.height }); // 814 is left align, 620 is down align
+
+        static int end_program, start_game;
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && hover_new_game)
+        {
+            start_game++;
+        }
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && hover_quit)
+        {
+            end_program++;
+        }
+        if (end_program > 0) // close the program if quit is pressed (end_program is bigger than 0)
+        {
+            return 0;
+        }
+        if (start_game > 0) // continue to the main program if new game is pressed (start_game is bigger than 0)
+        {
+            break;
+        }
+
+        EndDrawing();
+
+    }
+    while (!WindowShouldClose())
+    {
+
+        BeginDrawing();
+
+       
+
+        EndDrawing();
+    }
+
+    UnloadTexture(background);
+    UnloadTexture(stats);
+    UnloadTexture(Sun);
+    UnloadTexture(Mercury);
+    UnloadTexture(Venus);
+    UnloadTexture(Earth);
+    UnloadTexture(Mars);
+    UnloadTexture(Jupiter);
+    UnloadTexture(Saturn);
+    UnloadTexture(Uranus);
+    UnloadTexture(Neptune);
+    UnloadTexture(blank_info);
+    UnloadTexture(start);
+    UnloadTexture(new_game);
+    UnloadTexture(quit);
+    UnloadTexture(info);
+    UnloadTexture(menu);
+    UnloadTexture(functions);
+    UnloadTexture(checker);
+    UnloadTexture(Checker);
+    UnloadTexture(exit_button);
+    UnloadTexture(checker_exit_button);
+    UnloadTexture(exit_menu);
+    UnloadTexture(options_checker);
+    UnloadTexture(pluton);
+    UnloadTexture(add_planet_menu);
+    UnloadTexture(cancel_add_planet);
+    UnloadTexture(cancel_add_planet_blank);
+    UnloadTexture(pluton_in_stats);
+}
+
+
+
+  
+
+
+  
+
+    
+   
+   
+    
+
+
+        
+
+
+        
+  

6a129756a1e9c22890837916ffdd885386a61edb
DKrKostadinov22 <132456989+DKrKostadinov22@users.noreply.github.com>
2024-02-18 16:37:26 +0200

    Update UniBox.cpp

--- UniBox/UniBox.cpp
+++ UniBox/UniBox.cpp
@@ -155,12 +155,12 @@ Vector2 rotate_point(float& angle, float a, float e, float perihelionAngle, floa
 }
 Vector2 rotate_point_back(float& angle, float a, float e, float perihelionAngle, float speed)
 {
-    float radians = (angle + perihelionAngle) * DEG2RAD;
-    float radius = a * (1 - e * e) / (1 + e * cos(radians - perihelionAngle));
-    angle -= speed;
+    float radians = (angle + perihelionAngle) * DEG2RAD; // calculates the angle in radians
+    float radius = a * (1 - e * e) / (1 + e * cos(radians - perihelionAngle)); // calculates the radius
+    angle -= speed; // increments the angle by adding the rotation speed
     if (angle < 0.f)
         angle = 360.f + angle;
-    return Vector2{ screenWidth / 2.f + radius * float(cos(radians)), screenHeight / 2.f + radius * float(sin(radians)) };
+    return Vector2{ screenWidth / 2.f + radius * float(cos(radians)), screenHeight / 2.f + radius * float(sin(radians)) }; // returns x and y
 }
 void rotate_planet(float& angle, float a, float e, float perihelionAngle, float speed, int& left, int& down)
 {

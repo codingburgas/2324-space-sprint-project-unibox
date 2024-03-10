a4f7643d3e072ea89339c758ca7de2cf640d801d
AILalev22 <132459581+AILalev22@users.noreply.github.com>
2024-03-11 01:56:59 +0200

    Update StatsMenu.h

--- UniBox/Includes/StatsMenu.h
+++ UniBox/Includes/StatsMenu.h
@@ -1,3 +1,4 @@
+#pragma once
 #include "raylib.h"
 #include <algorithm>
 void stats_show(Texture2D, Texture2D, Texture2D, Texture2D, Texture2D, Texture2D); // show menu if clicked on info button and hover on the planets to see their stats
